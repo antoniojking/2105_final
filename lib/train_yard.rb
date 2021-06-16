3c5c85cc22fc1fb4ec0e94005fdd6bf21c994db3
Antonio King <antonio.j.king@gmail.com>
2021-06-16 10:02:07 -0600

    Create weight test for train yard class

--- lib/train_yard.rb
+++ lib/train_yard.rb
@@ -1,7 +1,13 @@
 class TrainYard
-  attr_reader :location
+  attr_reader :location,
+              :trains
 
   def initialize(info)
     @location = info[:location]
+    @trains = []
+  end
+
+  def add_train(train)
+    @trains << train 
   end
 end
