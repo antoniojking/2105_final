4377808ad752680be26b8aa74073b35885fa240d
Antonio King <antonio.j.king@gmail.com>
2021-06-16 09:08:33 -0600

    Create exists with attr test for car class

---
+++ spec/car_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require './lib/car'
+
+describe Car do
+  it 'exists with attributes' do
+    car1 = Car.new({type: 'Mail', weight: 5})
+
+    expect(car1).to be_an(Car)
+    expect(car1.type).to eq('Mail')
+    expect(car1.weight).to eq(5)
+  end
+end
